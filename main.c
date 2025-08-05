f9c298551d7477e785dcc3aa6f1545e9ffb2be4e
Muhammad Ali Khalid <khalidmuhammadali822@gmail.com>
2025-08-05 15:21:27 +0500

    addedwebrtc_vad

--- main.c
+++ main.c
@@ -1,39 +1,64 @@
-#define YIN_IMPLEMENTATION
-#include "yin.h"
+#include "build/yin.c"
+#include "build/webrtc_vad.h"
+#define DR_WAV_IMPLEMENTATION
+#include "build/dr_wav.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 #define HEADER_SIZE 44
+#define yin_SAMPLE_SIZE 2048
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Usage: ./pitch input.wav\n");
         return 1;
-    }
-
-    FILE *input = fopen(argv[1], "rb");
-    if (input == NULL) {
-        printf("Could not open file.\n");
+    }   
+ 
+    drwav wav;
+    if (!drwav_init_file(&wav, argv[1], NULL)) {
+        printf("Failed to open WAV file.\n");
         return 1;
     }
 
-    uint8_t header[HEADER_SIZE];
-    fread(&header, sizeof(header), 1, input);
 
     int16_t buffer[yin_SAMPLE_SIZE];
-    float norBuffer[yin_SAMPLE_SIZE];
     float pitchs[2000];
     int i = 0;
+    Yin yin;
+    Yin_init(&yin, yin_SAMPLE_SIZE, 0.2f);
 
-    while (fread(buffer, sizeof(buffer), 1, input)) {
-        int16_to_float_normalized(buffer, norBuffer, yin_SAMPLE_SIZE);
-        removeDC(norBuffer, yin_SAMPLE_SIZE);
-        pitchs[i] = yin_getFreq(norBuffer);
-        // printf("%f\t", pitchs[i]);
+    VadInst* handle = WebRtcVad_Create(); 
+    if (WebRtcVad_Init(handle) != 0) {
+        printf("Webrtc VAD failed/\n");
+    }
+    if (WebRtcVad_set_mode(handle, 1) != 0) {
+        printf("Webrtc VAD failed/\n");
+    }
+    int not_found = 0;
+    clock_t start = clock();
+    while (1) {
+        drwav_uint64 framesRead = drwav_read_pcm_frames_s16(&wav, yin_SAMPLE_SIZE, buffer);
+        if (framesRead == 0)
+            break;
+        // to deal with limited support of webrtc vad, middle of buffer is passed to it
+        if (WebRtcVad_Process(handle, 48000, buffer+304, 1440) != 1) {
+            printf("i: %d\n", i);
+            continue;
+        } 
+        removeDC(buffer, yin_SAMPLE_SIZE);
+		pitchs[i] = Yin_getPeriod(&yin, buffer);
+        printf("pitch: %f, i: %d\n", pitchs[i], i);
+        if (pitchs[i] == -1) {
+            not_found++;
+            continue;
+        }
         ++i;
     }
-
-    printf("\npitchs: %d\nMean: %lf\nMedian: %lf\n", i, calculateMean(pitchs, i), calculateMedian(pitchs, i));
-    fclose(input);
+    clock_t end = clock();
+    double elapsed_ms = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;printf("Elapsed time: %.2f ms\n", elapsed_ms);
+    printf("\npitchs: %d\nnot found: %d\nMean: %lf\nMedian: %lf\n", i, not_found, calculateMean(pitchs, i), calculateMedian(pitchs, i));
+    WebRtcVad_Free(handle);
+    drwav_uninit(&wav);
     return 0;
 }
--- volume/volume.c
+++ volume/volume.c
@@ -1,58 +1,67 @@
+// Modifies the volume of an audio file
+#define DR_WAV_IMPLEMENTATION
+#include "build/dr_wav.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define BLOCK_SIZE 2048
 
+// Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
 int main(int argc, char *argv[])
 {
+    // Check command-line arguments
     if (argc != 4)
     {
         printf("Usage: ./volume input.wav output.wav factor\n");
         return 1;
     }
 
-    FILE *input = fopen(argv[1], "rb");
-    if (input == NULL)
-    {
-        printf("Could not open %s.\n", argv[1]);
+    // Open files and determine scaling factor
+    drwav wav;
+    if (!drwav_init_file(&wav, argv[1], NULL)) {
+        printf("Failed to open WAV file.\n");
         return 1;
     }
-    FILE *values = fopen("values.txt", "wb");
-    FILE *output = fopen(argv[2], "wb");
-    if (output == NULL)
-    {
-        printf("Could not open %s.\n", argv[2]);
-        fclose(input);
+
+    drwav_data_format format;
+    format.container = drwav_container_riff;
+    format.format = DR_WAVE_FORMAT_PCM;
+    format.channels = wav.channels;
+    format.sampleRate = wav.sampleRate;
+    format.bitsPerSample = wav.bitsPerSample;
+
+    drwav out;
+    if (!drwav_init_file_write(&out, "output.wav", &format, NULL)) {
+        printf("Failed to create output.wav\n");
         return 1;
     }
+
 
     float factor = atof(argv[3]);
 
-    uint8_t header[HEADER_SIZE];
-    if (fread(header, HEADER_SIZE, 1, input) != 1) {
-        printf("Failed to read WAV header.\n");
-        fclose(input);
-        fclose(output);
-        return 1;
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer[BLOCK_SIZE];
+    while (1) {
+    drwav_uint64 framesRead = drwav_read_pcm_frames_s16(&wav, BLOCK_SIZE, buffer);
+    if (framesRead == 0)
+        break;
+
+    // Optionally apply processing here, e.g., volume *= 2
+    for (int i = 0; i < framesRead; i++) {
+        int32_t temp = buffer[i] * 2;  // example volume change
+        if (temp > 32767) temp = 32767;
+        if (temp < -32768) temp = -32768;
+        buffer[i] = temp;
     }
 
-    if (fwrite(header, HEADER_SIZE, 1, output) != 1) {
-        printf("Failed to write WAV header.\n");
-        fclose(input);
-        fclose(output);
-        return 1;
-    }
+    drwav_write_pcm_frames(&out, framesRead, buffer);
+}
 
-    int16_t buffer;
-    while (fread(&buffer, sizeof(int16_t), 1, input) == 1)
-    {
-        buffer = buffer * factor;
-        fprintf(values, "  %d", buffer);
-        fwrite(&buffer, sizeof(int16_t), 1, output);
-    }
 
-    fclose(input);
-    fclose(output);
-    return 0;
+    // Close files
+    drwav_uninit(&wav);
+    drwav_uninit(&out);
+
 }
